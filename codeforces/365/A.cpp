fbd9ba8851ae9eb3f5f3d03991e04acdefe675b8
sanjana-302 <sanjana.maheshwari456@gmail.com>
2021-03-28 13:34:00 +0530

    Add solution for problem `A - Good Number` Link: https://codeforces.com/contest/365/problem/A Tags: implementation, *1100 Ref: https://codeforces.com/contest/365/submission/111264260

---
+++ codeforces/365/A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("pin.txt", "r", stdin);
+    // freopen("pout.txt", "w", stdout);
+    int n, p;
+    cin >> n >> p;
+    int count = 0;
+    while (n--)
+    {
+        vector<int> check(p + 1, 0);
+        long long a;
+        cin >> a;
+        // if (a == 0)
+        // {
+        //     check[0] = 1;
+        // }
+        while (a)
+        {
+            if (a % 10 <= p)
+            {
+                check[a % 10] = 1;
+            }
+
+            a = a / 10;
+        }
+        bool yes = true;
+        for (int i = 0; i <= p; i++)
+        {
+            if (check[i] != 1)
+            {
+                yes = false;
+                break;
+            }
+        }
+        count += yes;
+    }
+
+    cout << count << endl;
+    return 0;
+}
